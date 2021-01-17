5a496289b3d87e694d68af8edaf3bf3280c5ed9e
pereiradaniel <danielpaulpereira@protonmail.com>
2021-01-17 16:33:32 -0500

    Fix example files.

--- Ext_linkage_Module_b.cpp
+++ Ext_linkage_Module_b.cpp
@@ -1,7 +1,7 @@
 // In the following program the name share_me refers to the same entity across two translation units.The variable share_me is accessed in Ext_linkage_Module_a.cpp and defined in Ext_linkage_Module_b.cpp
 
 // External linkage
-// Ext_linkage_Module_a.cpp
+// Ext_linkage_Module_b.cpp
 
 //#include <iostream>
 //void display();
--- Int_linkage_Module_b.cpp
+++ Int_linkage_Module_b.cpp
@@ -1,3 +1,5 @@
+// In the following program the name share_me refers to the same entity across two translation units.The variable share_me is accessed in Ext_linkage_Module_a.cpp and defined in Ext_linkage_Module_b.cpp
+
 // Internal Linkage
 // Int_linkage_Module_b.cpp
 

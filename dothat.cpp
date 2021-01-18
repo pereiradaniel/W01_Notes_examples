d0490e4073332c6c7a1eb2a76f3c8e94673b4af1
pereiradaniel <danielpaulpereira@protonmail.com>
2021-01-17 22:01:31 -0500

    Add namespaces.cpp and run

--- dothat.cpp
+++ dothat.cpp
@@ -7,15 +7,15 @@
 //	hat::leader(s);  // OK calls the leader() function declared in hat
 //}
 
-#include "dot.h"
-#include "hat.h"
-
-int main() {
-	char s[51];
-
-	using namespace dot;
-	leader(s);  // OK calls the function declared in dot
-
-	using namespace hat;
-	hat::leader(s);  // resolves the ambiguity in favour of hat
-}
+//#include "dot.h"
+//#include "hat.h"
+//
+//int main() {
+//	char s[51];
+//
+//	using namespace dot;
+//	leader(s);  // OK calls the function declared in dot
+//
+//	using namespace hat;
+//	hat::leader(s);  // resolves the ambiguity in favour of hat
+//}
